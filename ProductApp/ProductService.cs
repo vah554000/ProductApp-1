4f1a57919053df746ff7caa3772dc1c78f85c513
vah554000 <vah554000@gmail.com>
2023-11-02 22:23:36 +1100

    Update ProductService.cs

--- ProductApp/ProductService.cs
+++ ProductApp/ProductService.cs
@@ -7,7 +7,7 @@ namespace ProductApp
 {
     public class ProductService
     {
-        private static string db_connectionstring = "server=appserver6008089.database.windows.net;user=sqladmin;password=Azure@123;database=appdb";
+        private static string db_connectionstring = "server=appserver6008089554.database.windows.net;user=sqladmin;password=Azure@123;database=appdb";
 
         private SqlConnection GetConnection()
         {
